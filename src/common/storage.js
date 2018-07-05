ba847f924ed985e32022f212b7eb2c102699139a
daben <dabennn07@gmail.com>
2018-07-05 14:03:02 +0800

    perf: update Storage

--- src/common/storage.js
+++ src/common/storage.js
@@ -1,4 +1,4 @@
-import { isObject } from '../type';
+import { isString, isObject } from '../type';
 
 export default class Storage {
   constructor(key) {
@@ -9,16 +9,41 @@ export default class Storage {
       throw new Error('Empty String is not allow');
     }
     this.STORAGE_KEY = key;
+
+    this._init();
+  }
+
+  static setItem(...args) {
+    localStorage.setItem(...args);
+  }
+
+  static getItem(...args) {
+    return localStorage.getItem(...args);
+  }
+
+  static removeItem(...args) {
+    localStorage.removeItem(...args);
+  }
+
+  static clearAll() {
+    localStorage.clear();
+  }
+
+  _init() {
+    const value = localStorage[this.STORAGE_KEY];
+    if (!value) {
+      Storage.setItem(this.STORAGE_KEY, '');
+    }
   }
 
   add(obj = {}) {
     if (!isObject(obj)) throw new TypeError('Invalid Object: ' + obj);
     try {
-      const storage = localStorage.getItem(this.STORAGE_KEY);
+      const storage = Storage.getItem(this.STORAGE_KEY);
       if (storage) {
         obj = Object.assign(JSON.parse(storage), obj);
       }
-      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(obj));
+      Storage.setItem(this.STORAGE_KEY, JSON.stringify(obj));
     } catch (e) {
       throw new Error(e);
     }
@@ -28,18 +53,18 @@ export default class Storage {
   set(key, value) {
     const storage = this.get();
     storage[key] = value;
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(storage));
+    Storage.setItem(this.STORAGE_KEY, JSON.stringify(storage));
     return this;
   }
 
   get(key = '') {
     if (!isString(key)) return null;
     try {
-      const storage = JSON.parse(localStorage.getItem(this.STORAGE_KEY)) || {};
+      const storage = JSON.parse(Storage.getItem(this.STORAGE_KEY)) || {};
       if (!key) return storage;
       return storage[key];
     } catch (e) {
-      console.error(e);
+      console.warn(e);
       return {};
     }
   }
@@ -47,11 +72,11 @@ export default class Storage {
   remove(key = '') {
     const storage = this.get();
     delete storage[key];
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(storage));
+    Storage.setItem(this.STORAGE_KEY, JSON.stringify(storage));
     return this;
   }
 
   clear() {
-    localStorage.removeItem(this.STORAGE_KEY);
+    Storage.removeItem(this.STORAGE_KEY);
   }
 }
