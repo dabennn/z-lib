d0e4e18f2f65917f8d06d5c95ed34e17cc176054
daben <dabennn07@gmail.com>
2018-07-27 14:43:52 +0800

    feat: add isObjectLike deepClone methods

--- src/common/index.js
+++ src/common/index.js
@@ -1,6 +1,7 @@
 export * from './string';
 export * from './number';
 export * from './array';
+export * from './object';
 export * from './date';
 export { default as Storage } from './storage';
 export { default as ImageLoader } from './image';
--- src/type/object.js
+++ src/type/object.js
@@ -27,6 +27,17 @@ export function isObject(obj) {
 };
 
 /**
+ * 是否是typeof值为'object'或'function'的对象
+ * @author   daben<dabennn07@gmail.com>
+ * @param    {*}                   obj  The value to check.
+ * @return   {Boolean}             'true' if value is ObjectLike, else 'false'.
+ */
+export function isObjectLike(obj) {
+  const type = typeof obj;
+  return obj !== null && (type === 'object' || type === 'function');
+};
+
+/**
  * 是否是空对象
  * @author   daben<dabennn07@gmail.com>
  * @param    {*}                   obj  The value to check.
