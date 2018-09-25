69ee2eb8658280779102fcbe2f82365b9c95b9d6
daben <dabennn07@gmail.com>
2018-09-25 18:29:55 +0800

    feat: 支持按需加载

--- script/build-lib.js
+++ script/build-lib.js
@@ -1,38 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const ora = require('ora');
+const { traversal } = require('./util.js');
+const { IGNORE_FILES } = require('./config');
 const srcPath = path.resolve(__dirname, '../src');
 const libPath = path.resolve(__dirname, '..', 'lib');
+
 const spinner = ora('Start Building...');
 
-function traversal(src, filter = () => {}) {
-  const files = [];
-  const dirList = fs.readdirSync(src, 'utf8');
-  dirList.forEach(item => {
-    const _path = path.join(src, item);
-    if (fs.statSync(_path).isFile()) {
-      if (typeof filter === 'function' && !filter(_path)) return;
-      files.push(_path);
-    } else {
-      if (/node_modules/.test(_path)) return;
-      const _files = traversal(_path, filter);
-      files.push(..._files);
-    }
-  });
-  return files;
-}
-
 spinner.start();
+
 const filesPath = traversal(srcPath, (src) => {
-  const ignoreFiles = [
-    '.DS_Store',
-    'LICENSE',
-    'lib/index.js',
-    'src/index.js',
-    'src/root.js',
-    'constants.js',
-  ];
-  const regex = ignoreFiles.reduce((a, b) => a + '|' + b).replace(/\./g, '\\.');
+  const regex = IGNORE_FILES.reduce((a, b) => a + '|' + b).replace(/\./g, '\\.');
   return !new RegExp(regex).test(src);
 });
 
@@ -44,7 +23,7 @@ fs.mkdir(libPath, () => {
     spinner.text = `Building File: ${filename}`;
 
     let content = fs.readFileSync(file, { encoding: 'utf8' });
-    content = content.replace(/(import.*?)'(.*?)'/gm, (s, s1, s2) => {
+    content = content.replace(/(from\s)'(.*?)'/gm, (s, s1, s2) => {
       s2 = /\//.test(s2) ? s2.replace(/.*(\/\w*$)/, '.$1') : './' + s2;
       return `${s1}'${s2}'`;
     });
@@ -52,6 +31,5 @@ fs.mkdir(libPath, () => {
     fs.writeFileSync(writePath, content, { encoding: 'utf8' });
   });
 });
-
 
 spinner.stop();
---
+++ script/config.js
@@ -0,0 +1,12 @@
+const IGNORE_FILES = [
+  '.DS_Store',
+  'LICENSE',
+  'lib/index.js',
+  'src/index.js',
+  'src/root.js',
+  'constants.js',
+];
+
+module.exports = {
+  IGNORE_FILES,
+};
