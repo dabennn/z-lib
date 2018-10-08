f78cbd25e9d66469ddf081e09f6343f85ab352d5
daben <dabennn07@gmail.com>
2018-10-08 17:58:43 +0800

    feat: add methods get and isSymbol

---
+++ src/common/get.js
@@ -0,0 +1,21 @@
+/**
+ * 链式取值
+ * @param {Object} obj 数据源
+ * @param {String|Array} props key
+ * @param {Any} def 默认值
+ */
+export default function get(obj, props, def) {
+  props = typeof props === 'string' ? props.split(/\.|\[|\]/) : props;
+
+  const len = props.length;
+  let ret = obj;
+
+  for (let i = 0; i < len; i++) {
+    const prop = props[i];
+    if (prop === '') continue;
+    if (ret === undefined) return def;
+    ret = ret[prop];
+  }
+
+  return ret || def;
+};
